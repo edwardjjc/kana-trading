b95b4bc8837efb1c4fea03a03e13ec4edc3b94f0
Edward Jimenez <edwardjjc@Edwards-MacBook-Pro.local>
2018-07-24 16:45:49 -0400

    commit numero 2

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css']
 })
-export class ContactsComponent implements OnInit {
+export class ContactsComponent {
   
   contactos: Observable<any[]>;
 
   constructor(db: AngularFirestore) {
     this.contactos = db.collection('Contactos').valueChanges();
   }
-  ngOnInit() {
+
+  enviarComentario(){
+    alert('Mensaje Enviado');
   }
 
 }
