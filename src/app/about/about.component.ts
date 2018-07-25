c8f227bfeaa833045aeaa75dcd677b93d01795b4
Edward Jimenez <edwardjjc@Edwards-MacBook-Pro.local>
2018-07-25 19:41:38 -0400

    err

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -13,17 +13,6 @@ export class AboutComponent implements OnInit {
   }  
 
   ngOnInit() {
-    this.initMap();
-  }
-
-  // Initialize and add the map
-  initMap() {
-    // The location of Uluru
-    var uluru = {lat: 18.510850, lng: -69.849097};
-    // The map, centered at Uluru
-    var map = new google.maps.Map(document.getElementById('map'), {zoom: 15, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
   }
 
 }
